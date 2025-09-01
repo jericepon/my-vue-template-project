09237e8eab5d36bb07ac056494f78352f7602544
Jeric Epon <jericepon@gmail.com>
2025-09-01 21:45:13 +0800

    Implement modal functionality with dynamic content and multiple instances

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
-import Button from './components/Button/Button.vue'
+import Button from '@/components/Button/Button.vue'
+import Modal from '@/components/Modal/Modal.vue'
+import { useModal } from './composables/useModal'
+import ModalTestContent from './components/ModalTestContent.vue'
+
+const { open } = useModal()
+const toggleModal = () => {
+  open({
+    title: 'Example Modal',
+    content: '<p>This is an example modal content.</p>',
+  })
+}
+
+const toggle2ndModal = () => {
+  open({
+    title: 'Second Modal',
+    content: '<p>This is the content of the second modal.</p>',
+    component: ModalTestContent,
+  })
+}
 </script>
 
 <template>
-  <Button color="primary">Click Me</Button>
+  <Button color="primary" @click="toggleModal">Modal 1</Button>
+  <Button color="primary" @click="toggle2ndModal">Modal 2</Button>
+  <Modal />
 </template>
 
 <style scoped>
---
+++ src/components/ModalTestContent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useModal } from '@/composables/useModal'
+const { open } = useModal()
+const toggleModal = () => {
+  open({
+    title: 'Example Modal',
+    content: '<p>This is an example modal content.</p>',
+    disableOutsideClick: true,
+  })
+}
+</script>
+<template>
+  <div>
+    <h3>Modal Test Content</h3>
+    <p>This is a test content for the modal.</p>
+    <p>It can include any Vue component or HTML content.</p>
+    <p>Feel free to customize it as needed!</p>
+    <Button @click="toggleModal">Toggle 2nd modal</Button>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
